b7be20e61449cec910ca50ad8d74e8e385628be5
Snoz58 <corentin.metenier@gmail.com>
2018-08-09 15:43:13 +0200

    Doc + code NRF24

---
+++ Arduino/NRF24_reception/NRF24_reception.ino
@@ -0,0 +1,36 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  pinMode(A1, OUTPUT); 
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    if (text[0] == '1'){
+      Serial.println("LEDON");
+      digitalWrite(A1, HIGH);
+    }
+    else {
+      Serial.println("LEDOFF");
+      digitalWrite(A1, LOW);
+    }
+    Serial.println(text);
+  }
+}
