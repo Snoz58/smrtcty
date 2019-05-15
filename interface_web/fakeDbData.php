c0a8262cd5038580265aef2b8dda3028dd25a537
Snoz58 <corentin.metenier@gmail.com>
2019-05-15 15:53:48 +0200

    Corrections / choix du mois de début / fin

--- interface_web/fakeDbData.php
+++ interface_web/fakeDbData.php
@@ -6,10 +6,20 @@ VALUES<br />");
 $debut = 50;
 $pasMax = 10;
 
-for ($m = 10; $m <= 12; $m++){ // $m = mois
+$moisDebut = 1;
+$moisFin = 12;
+
+
+for ($m = $moisDebut; $m <= $moisFin; $m++){ // $m = mois
 	for ($j = 1; $j <= 30; $j ++){ // $j = jour
 		for ($h = 6; $h <= 22; $h +=2){ // $h = heure
-		echo('("2019-'.$m.'-'.$j.' '.$h.':00:00", '.$debut.', 1, 8, 2),'."<br />");
+
+			// Correctif : ajout d'un 0 devant les chiffres (01, 02, 03, [...], 09)
+			$mstring = ($m < 10) ? '0'.$m : (string)$m;
+			$hstring = ($h < 10) ? '0'.$h : (string)$h;
+
+			echo('("2019-'.$mstring.'-'.$j.' '.$hstring.':00:00", '.$debut.', 1, 8, 2),'."<br />");
+
 		modifchiffre($debut);
 		}
 	}
